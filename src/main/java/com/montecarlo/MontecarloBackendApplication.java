1e76e18c30c8d9964d8b6f4cc3c63fb344898a65
Rafiqui12345 <caballerot_3@hotmail.com>
2026-05-20 17:36:38 -0500

    chore(init): initialize Spring Boot backend project

---
+++ src/main/java/com/montecarlo/MontecarloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.montecarlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontecarloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontecarloBackendApplication.class, args);
+	}
+
+}
